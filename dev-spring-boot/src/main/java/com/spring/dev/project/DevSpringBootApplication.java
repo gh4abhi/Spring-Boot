7cae5ed60cb3862a190eac09495ffab46fa699a2
gh4abhi <gh4abhi@gmail.com>
2024-07-04 15:27:14 +0530

    Added starter spring boot code.

---
+++ dev-spring-boot/src/main/java/com/spring/dev/project/DevSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dev.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringBootApplication.class, args);
+	}
+
+}
